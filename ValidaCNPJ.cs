cd135b24f8effa8a663d8d9ff2bdc4cace39fbc5
Randal Junior <randal.junior@gmail.com>
2025-05-28 10:06:51 -0300

    Bug e Ajustes

--- ValidaCNPJ.cs
+++ ValidaCNPJ.cs
@@ -1,12 +1,5 @@
 ﻿using MyExtensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using ZLinq.Simd;
 
 namespace ValidarCPFeCNPJ;
 
